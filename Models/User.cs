9bace44c37c71f13e975a80987d633d105ebbf65
Suporte3 <suporte3@fingertech.com.br>
2022-08-01 11:55:21 -0300

    Add Authorization

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiFingertec3._0.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        //public string Role { get; set; }
+    }
+}
